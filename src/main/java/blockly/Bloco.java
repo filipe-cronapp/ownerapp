c56332b92498e65401c29f1f1ce00b17bdd4ebe2
Filipe Pereira <filipe.pereira@techne.com.br>
2019-08-01 19:55:45 +0000

    Alteração nos blocos

--- src/main/java/blockly/Bloco.java
+++ src/main/java/blockly/Bloco.java
@@ -2,6 +2,7 @@ package blockly;
 
 import cronapi.*;
 import cronapi.rest.security.CronappSecurity;
+import java.util.Iterator;
 import java.util.concurrent.Callable;
 
 
@@ -20,14 +21,16 @@ public static Var Executar() throws Exception {
  return new Callable<Var>() {
 
    private Var objetoJson = Var.VAR_NULL;
-   private Var url = Var.VAR_NULL;
+   private Var listas = Var.VAR_NULL;
+   private Var i = Var.VAR_NULL;
 
    public Var call() throws Exception {
-    if (cronapi.logic.Operations.isNullOrEmpty(objetoJson).getObjectAsBoolean()) {
-        url = Var.valueOf(Var.valueOf("https://my-api-strapi.herokuapp.com/produtos").toString());
-        objetoJson = cronapi.json.Operations.toJson(cronapi.util.Operations.getURLFromOthers(Var.valueOf("GET"), Var.valueOf("application/json"), url, Var.VAR_NULL, Var.VAR_NULL, Var.VAR_NULL));
-    }
-    cronapi.util.Operations.callClientFunction(Var.valueOf("cronapi.screen.changeValueOfField"), Var.valueOf("Produto.active.categoria_nome"), cronapi.json.Operations.getJsonOrMapField(objetoJson, Var.valueOf("[0].nome")));
+    objetoJson = cronapi.util.Operations.getURLFromOthers(Var.valueOf("GET"), Var.valueOf("application/json"), Var.valueOf("https://my-api-strapi.herokuapp.com/produtos"), Var.VAR_NULL, Var.VAR_NULL, Var.VAR_NULL);
+    listas = cronapi.json.Operations.toList(objetoJson);
+    for (Iterator it_i = objetoJson.iterator(); it_i.hasNext();) {
+        i = Var.valueOf(it_i.next());
+        System.out.println(cronapi.object.Operations.getObjectField(i, Var.valueOf("nome")).getObjectAsString());
+    } // end for
     return Var.VAR_NULL;
    }
  }.call();
