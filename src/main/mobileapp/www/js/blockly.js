45f7296dc14d2ce663dab4de778a4272c27e4291
Jonas Barbosa <jonas.barbosa@cronapp.io>
2019-08-05 17:30:16 +0000

    edit bar assertive com overide para theme novo.

--- src/main/mobileapp/www/js/blockly.js
+++ src/main/mobileapp/www/js/blockly.js
@@ -1,1 +1,12 @@
-window.blockly = window.blockly || {};window.blockly.js = window.blockly.js || {};window.blockly.js.blockly = window.blockly.js.blockly || {};window.translations = window.translations || {};window.translations.localesKeys = ['en_us','pt_br'];window.translations.localesRef =  {"en*":"en_us","*":"pt_br","pt*":"pt_br"};document.write("<script src='js/blockly/auth/Home.blockly.js?1564672393598'></script>");document.write("<script src='js/blockly/auth/ChangePassword.blockly.js?1564672393601'></script>");document.write("<script src='js/blockly/auth/Login.blockly.js?1564672393596'></script>");
+window.fixedTimeZone = true;
+window.timeZone = 'UTC';
+window.timeZoneOffset = 0;
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.translations = window.translations || {};
+window.translations.localesKeys = ['en_us','pt_br'];
+window.translations.localesRef =  {"en*":"en_us","*":"pt_br","pt*":"pt_br"};
+document.write("<script src='js/blockly/auth/Home.blockly.js?1564672393598'></script>");
+document.write("<script src='js/blockly/auth/ChangePassword.blockly.js?1564672393601'></script>");
+document.write("<script src='js/blockly/auth/Login.blockly.js?1564672393596'></script>");
